cc3397447e76ebbe9b6dc362184f5ad8c4945514
alishamsa <168437003+alishamsa@users.noreply.github.com>
2025-11-15 15:23:49 +0300

    Update proc_load_bronze.sql

--- scripts/bronze_layer/proc_load_bronze.sql
+++ scripts/bronze_layer/proc_load_bronze.sql
@@ -42,7 +42,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[crm_cust_info];
         PRINT '>>Inserting table: crm_cust_info';
         BULK INSERT [bronze].[crm_cust_info]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        FROM '../../datasets/cust_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -59,7 +59,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[crm_prd_info];
         PRINT '>>Inserting table: crm_prd_info';
         BULK INSERT [bronze].[crm_prd_info]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        FROM '../../datasets/prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -76,7 +76,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[crm_sales_details];
         PRINT '>>Inserting table: crm_sales_details';
         BULK INSERT [bronze].[crm_sales_details]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        FROM '../../datasets/sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -100,7 +100,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[erp_cust_az12];
         PRINT '>>Inserting table: erp_cust_az12';
         BULK INSERT [bronze].[erp_cust_az12]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+        FROM '../../datasets/cust_az12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -117,7 +117,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[erp_loc_a101];
         PRINT '>>Inserting table: erp_loc_a101';
         BULK INSERT [bronze].[erp_loc_a101]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        FROM '../../datasets/loc_a101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -134,7 +134,7 @@ BEGIN
         TRUNCATE TABLE [bronze].[erp_px_cat_g1v2];
         PRINT '>>Inserting table: erp_px_cat_g1v2';
         BULK INSERT [bronze].[erp_px_cat_g1v2]
-        FROM 'C:\sql-ultimate-course\datawarehouse project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+        FROM '../../datasets/px_cat_g1v2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
